2cdb499debdf1b9ae9889811dc7b7f4f69af5d2a
Carlos Guerrero <91098215+imcarlosguerrero@users.noreply.github.com>
2022-11-27 23:59:59 +0000

    ok

--- app.py
+++ app.py
@@ -1,31 +1,57 @@
 #Create a flask app
 
 from flask import Flask, render_template, request, session, redirect, url_for
+
+import numpy as np
+from PIL import Image
+
 from flask_session import Session
-import deployDevContainer
+
+UPLOAD_FOLDER = '/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
+
 app.config["SESSION_PERMANENT"] = False
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 app.secret_key = 'hardcoding is bad'
 sess = Session()
 SESSION_TYPE = 'filesystem'
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route("/", methods=["GET"])
 def index():
-    if(session.get('mensaje', "no set") == "no set"):
-        return render_template("index.html")
-    else:
-        mensaje = session["mensaje"]
-        session.clear()
-        return render_template("index.html", sendMessage = True, mensaje=mensaje)
+    return render_template("index.html")
 
-@app.route("/createContainer", methods=["POST"])
-def createContainer():
-    containerImage = str(request.form.get("containerImage"))
-    containerName = str(request.form.get("containerName"))
-    containerMemory = str(request.form.get("containerMemory"))
-    containerCPU = str(request.form.get("containerCPU"))
-    containerPassword = str(request.form.get("containerPassword"))
-    print(containerImage, containerName, containerMemory, containerCPU, containerPassword)
-    session['mensaje'] = deployDevContainer.createContainer(containerName, containerImage, containerMemory, containerCPU, containerPassword)
-    return redirect(url_for("index"))
+@app.route("/compressImage", methods=["POST"])
+def compressImage():
+    imageToCompress = request.files['imageToCompress']
+    imageToCompress.save('imageToCompress.png')
+    print(imageToCompress)
+    compressionFactor = request.form.get("compressionFactor")
+    
+    if imageToCompress and allowed_file(imageToCompress.filename):
+        
+        img = Image.open(imageToCompress)
+        imggray = img.convert('LA')
+        imgmat = np.array(list(imggray.getdata(band=0)), float)
+        imgmat.shape = (imggray.size[1], imggray.size[0])
+        imgmat = np.matrix(imgmat)
+        
+        U, sigma, V = np.linalg.svd(imgmat)
+        
+        for i in range(5, int(compressionFactor), 5):
+            reconstimg = np.matrix(U[:, :i]) * np.diag(sigma[:i]) * np.matrix(V[:i, :])
+            
+        #Save the image
+        img = Image.fromarray(reconstimg)
+        img = img.convert('RGB')
+        img.save("compressedImage" + compressionFactor + ".png")
+
+        return render_template("index.html", sendMessage=True, mensaje="Imágen comprimida exitosamente")
+    else:
+        return render_template("index.html", sendMessage=True, mensaje="Extensión de archivo no permitida, solo se permiten archivos .png, .jpg y .jpeg")
